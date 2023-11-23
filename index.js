c63237b83855b22c1d34654073dfa5cdb0b4bce7
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-11-23 18:43:51 -0300

    sound improved

--- index.js
+++ index.js
@@ -2,7 +2,39 @@ let drumArr = document.querySelectorAll(".drum");
 
 for(let i = 0; i < drumArr.length; i++){
     drumArr[i].addEventListener("click", function() {
-        this.style.color = "white"
+        switch(this.innerHTML){
+            case "w":
+                var som = new Audio("sounds/crash.mp3");
+                som.play();
+                break;
+            case "a":
+                var som = new Audio("sounds/kick-bass.mp3");
+                som.play();
+                break;
+            case "s" :
+                var som = new Audio("sounds/snare.mp3");
+                som.play();
+                break;
+            case "d":
+                var som = new Audio("sounds/tom-1.mp3");
+                som.play();
+                break;
+            case "j":
+                var som = new Audio("sounds/tom-2.mp3");
+                som.play();
+                break;
+            case "k" :
+                var som = new Audio("sounds/tom-3.mp3");
+                som.play();
+                break;
+            case "l" : 
+                var som = new Audio("sounds/tom-4.mp3");
+                som.play();
+                break;
+            default:
+                console.log(this.innerHTML);
+            
+        }
     });
 }
 
