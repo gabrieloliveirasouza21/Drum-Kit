a9065b90bcd10bfdb89f8425612b41a50ddacf49
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-11-23 19:19:56 -0300

    keyboard keys implemented

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 let drumArr = document.querySelectorAll(".drum");
 
+
 for(let i = 0; i < drumArr.length; i++){
     drumArr[i].addEventListener("click", function() {
-        switch(this.innerHTML){
-            case "w":
-                var som = new Audio("sounds/crash.mp3");
-                som.play();
-                break;
-            case "a":
-                var som = new Audio("sounds/kick-bass.mp3");
-                som.play();
-                break;
-            case "s" :
-                var som = new Audio("sounds/snare.mp3");
-                som.play();
-                break;
-            case "d":
-                var som = new Audio("sounds/tom-1.mp3");
-                som.play();
-                break;
-            case "j":
-                var som = new Audio("sounds/tom-2.mp3");
-                som.play();
-                break;
-            case "k" :
-                var som = new Audio("sounds/tom-3.mp3");
-                som.play();
-                break;
-            case "l" : 
-                var som = new Audio("sounds/tom-4.mp3");
-                som.play();
-                break;
-            default:
-                console.log(this.innerHTML);
-            
-        }
+        somzao(this.innerHTML);
     });
+}
+
+document.addEventListener("keydown", (event)=>{
+    somzao(event.key);
+});
+
+let somzao = (key) =>{
+
+    switch(key){
+        case "w":
+            var som = new Audio("sounds/crash.mp3");
+            som.play();
+            break;
+        case "a":
+            var som = new Audio("sounds/kick-bass.mp3");
+            som.play();
+            break;
+        case "s" :
+            var som = new Audio("sounds/snare.mp3");
+            som.play();
+            break;
+        case "d":
+            var som = new Audio("sounds/tom-1.mp3");
+            som.play();
+            break;
+        case "j":
+            var som = new Audio("sounds/tom-2.mp3");
+            som.play();
+            break;
+        case "k" :
+            var som = new Audio("sounds/tom-3.mp3");
+            som.play();
+            break;
+        case "l" : 
+            var som = new Audio("sounds/tom-4.mp3");
+            som.play();
+            break;
+        default:
+            console.log(this.innerHTML);
+        
+    }
 }
 
 // var sound = new Audio('sounds/tom-1.mp3');
